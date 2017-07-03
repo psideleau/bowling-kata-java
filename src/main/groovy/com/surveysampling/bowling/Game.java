c63af5346693966097a4f6a673d250b3156b38d4
Paul Sideleau <paul.sideleau@surveysampling.com>
2017-07-03 13:28:14 -0400

    attempt to map an object model

--- src/main/groovy/com/surveysampling/bowling/Game.java
+++ src/main/groovy/com/surveysampling/bowling/Game.java
@@ -1,42 +1,85 @@
 package com.surveysampling.bowling;
 
+import java.util.*;
+
 /**
  * Created by SSI.
  */
 public class Game {
+    private static final int BONUS_FRAME = 2;
+    private static final int TOTAL_PINS = 10;
     private static final int NUMBER_OF_FRAMES = 10;
-    private int[] rolls = new int[21];
-    private int currentRoll = 0;
+    public static final int NOT_ROLLED = -1;
+    private int currentFrame = 0;
+    private List<Frame> frames = new ArrayList<>(NUMBER_OF_FRAMES);
+
+    public Game() {
+        for (int frame = 0; frame < NUMBER_OF_FRAMES + BONUS_FRAME; frame++) {
+            frames.add(new Frame());
+        }
+    }
 
     public void roll(int pins) {
-        rolls[currentRoll++] = pins;
+        Frame frame = frames.get(currentFrame);
+
+        if (frame.roll1 == NOT_ROLLED) {
+            frame.roll1 = pins;
+            if (pins == TOTAL_PINS) {
+                currentFrame+=1;
+            }
+        }
+        else  {
+            frame.roll2 = pins;
+            currentFrame+=1;
+        }
     }
 
     public int getScore() {
         int totalScore = 0;
-        int frameIndex = 0;
 
-        for (int frame = 0; frame < NUMBER_OF_FRAMES; frame++) {
-            if (rolls[frameIndex] == 10) {
-                totalScore += 10 + rolls[frameIndex + 1] +  rolls[frameIndex + 2];
-                frameIndex++;
+        for (int frameIdx = 0; frameIdx < NUMBER_OF_FRAMES; frameIdx++) {
+            Frame frame = frames.get(frameIdx);
+            Frame nextFrame = frames.get(frameIdx + 1);
+
+            if (frame.isStrike()) {
+                totalScore += TOTAL_PINS + nextFrame.getRoll1();
+                totalScore += (nextFrame.isStrike()) ? frames.get(frameIdx + 2).getRoll1() : nextFrame.getRoll2();
             }
-            else if (isSpare(frameIndex)) {
-                totalScore += 10 + rolls[frameIndex + 2];
-                frameIndex+= 2;
+            else if (frame.isSpare()) {
+                totalScore += TOTAL_PINS + nextFrame.getRoll1();
             }
             else {
-                totalScore += rolls[frameIndex] + rolls[frameIndex + 1];
-                frameIndex+= 2;
+                totalScore += frame.getScore();
             }
-
-
         }
 
         return totalScore;
     }
 
-    private boolean isSpare(int i) {
-        return rolls[i] + rolls[i + 1] == 10;
+
+    public static class Frame {
+        private int roll1 = -1;
+        private int roll2 = -1;
+
+        boolean isStrike() {
+            return roll1 == TOTAL_PINS;
+        }
+
+        boolean isSpare() {
+            return (roll1 + roll2) == TOTAL_PINS;
+        }
+
+        int getScore() {
+            return  getRoll1() + getRoll2();
+        }
+
+        public int getRoll1() {
+            return roll1 == NOT_ROLLED ? 0 : roll1;
+        }
+
+        public int getRoll2() {
+            return roll2 == NOT_ROLLED ? 0 : roll2;
+        }
     }
+
 }
